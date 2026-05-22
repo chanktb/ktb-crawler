24cc7fad90f716fe2edf8844cb035372e5d27c67
Ktb <khuedhsp@gmail.com>
2026-05-22 10:12:26 +0700

    Add install script and ignore .env

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ==============================================
+echo 🚀 DANG CAI DAT CAC THU VIEN CAN THIET...
+echo ==============================================
+
+pip install -r requirements.txt
+
+echo.
+echo ==============================================
+echo ✅ CAI DAT THANH CONG!
+echo Ban co the chay "run_crawler.bat" de bat dau.
+echo ==============================================
+pause
